caaec59344abe2fdf44a3d9beee4b1f662a24a38
Egor <160damage@gmail.com>
2017-05-04 17:22:18 +0300

    js

--- youtube/js/common.js
+++ youtube/js/common.js
@@ -1,17 +1,73 @@
 /**
  * Created by Soff on 20.01.2017.
  */
-// toggle.click(function(e){
-//     e.preventDefault();
-//     e.stopPropagation();
 
-//     nav.toggleClass('navigation_open');
+$(function(){  
+    $('.footer__up').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
 
-//     $(document).one('click', function closeMenu (e){
-//         if(nav.has(e.target).length === 0){
-//             nav.removeClass('navigation_open');
-//         } else {
-//             $(document).one('click', closeMenu);
-//         }
-//     });
-// })
+    var header = $('.header'),
+    service = $('.service'),
+    promote = $('.promote'),
+    instruction = $('.instruction');
+
+
+var wrapper = $(".wrap-withoutfoot"),
+    $menu = $(".page-navigation"),
+    $window = $(window);
+
+$( document ).ready(function() {
+    if ($(window).scrollTop() == (header.offset().top )) {
+              $menu.children('.page-navigation__item').removeClass('page-navigation__item_active');
+        $('.page-navigation__item[href="#header"]').addClass('page-navigation__item_active');
+  }
+    else if ($(window).scrollTop() <= (service.offset().top + 300 )) {
+              $menu.children('.page-navigation__item').removeClass('page-navigation__item_active');
+        $('.page-navigation__item[href="#service"]').addClass('page-navigation__item_active');
+  } else if ($(window).scrollTop() <= (promote.offset().top + 300)) {
+              $menu.children('.page-navigation__item').removeClass('page-navigation__item_active');
+        $('.page-navigation__item[href="#promote"]').addClass('page-navigation__item_active');
+  } else if ($(window).scrollTop() <= (instruction.offset().top + 300)) {
+              $menu.children('.page-navigation__item').removeClass('page-navigation__item_active');
+        $('.page-navigation__item[href="#instruction"]').addClass('page-navigation__item_active');
+  }
+});
+
+$( window ).scroll(function() {
+    
+if ($(window).scrollTop() == (header.offset().top )) {
+              $menu.children('.page-navigation__item').removeClass('page-navigation__item_active');
+        $('.page-navigation__item[href="#header"]').addClass('page-navigation__item_active');
+  }
+    else if ($(window).scrollTop() <= (service.offset().top + 300 )) {
+              $menu.children('.page-navigation__item').removeClass('page-navigation__item_active');
+        $('.page-navigation__item[href="#service"]').addClass('page-navigation__item_active');
+  } else if ($(window).scrollTop() <= (promote.offset().top + 300)) {
+              $menu.children('.page-navigation__item').removeClass('page-navigation__item_active');
+        $('.page-navigation__item[href="#promote"]').addClass('page-navigation__item_active');
+  } else if ($(window).scrollTop() <= (instruction.offset().top + 300)) {
+              $menu.children('.page-navigation__item').removeClass('page-navigation__item_active');
+        $('.page-navigation__item[href="#instruction"]').addClass('page-navigation__item_active');
+  }
+  return false;
+});
+
+$menu.on("click","a", function(){
+    var $this = $(this),
+        href = $this.attr("href"),
+        topY = $(href).offset().top;
+   
+$("html, body").animate({
+            scrollTop: topY
+        }, 600);
+        $menu.children('.page-navigation__item').removeClass('page-navigation__item_active');
+        $this.addClass('page-navigation__item_active');
+  
+  return false;
+});  
+
+});
