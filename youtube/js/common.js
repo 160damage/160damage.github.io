c468783b6413db0602c2ddbaffabb513d026ca52
Egor <160damage@gmail.com>
2017-05-04 15:12:46 +0300

    yotube

---
+++ youtube/js/common.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Soff on 20.01.2017.
+ */
+// toggle.click(function(e){
+//     e.preventDefault();
+//     e.stopPropagation();
+
+//     nav.toggleClass('navigation_open');
+
+//     $(document).one('click', function closeMenu (e){
+//         if(nav.has(e.target).length === 0){
+//             nav.removeClass('navigation_open');
+//         } else {
+//             $(document).one('click', closeMenu);
+//         }
+//     });
+// })
