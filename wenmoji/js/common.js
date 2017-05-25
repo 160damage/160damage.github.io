d90a188bdd9c775d2210a7fcdb32fcbf913cf3d8
Egor <160damage@gmail.com>
2017-05-25 17:04:12 +0300

    responsive fix

--- wenmoji/js/common.js
+++ wenmoji/js/common.js
@@ -4,7 +4,7 @@ $('.carousel').slick({
     speed: 1000,
     slidesToShow: 1,
     autoplay: true,
-    autoplaySpeed: 1500,
+    autoplaySpeed: 2000,
     arrows: false,
     adaptiveHeight:true
 });
