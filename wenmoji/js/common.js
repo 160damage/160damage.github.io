b1d3f521a2e84f3eeb5cca3ff6437c2e87d918af
Egor <160damage@gmail.com>
2017-05-25 16:57:33 +0300

    wenmiju update

--- wenmoji/js/common.js
+++ wenmoji/js/common.js
@@ -4,7 +4,7 @@ $('.carousel').slick({
     speed: 1000,
     slidesToShow: 1,
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 1500,
     arrows: false,
     adaptiveHeight:true
 });
